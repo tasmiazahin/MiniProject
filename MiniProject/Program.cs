a883c44a9522b23ef6a382a3484e067d58750e7a
tasmia <tasmiamaithily@hotmail.com>
2023-03-10 15:59:30 +0100

    Updated README and added retry fuctionality

--- MiniProject/Program.cs
+++ MiniProject/Program.cs
@@ -11,86 +11,105 @@ class Program
         ConsoleKeyInfo key;
         int option = 1;
 
-        // Selection value of the menu
-        bool isSelected = false;
-
-        // Get cursor position
-        (int left, int top) = Console.GetCursorPosition();
-        string color = "✅  \u001b[32m";
-
-
-        Console.CursorVisible = false;
-
-       
-        while (!isSelected)
+        // Retry the menu again
+        bool isRunning = true;
+        while (isRunning)
         {
-            Console.SetCursorPosition(left, top);
+            // Selection value of the menu
+            bool isSelected = false;
 
-            Console.WriteLine($"\n{(option == 1 ? color : "    ")}Create Person \u001b[0m");
-            Console.WriteLine($"{(option == 2 ? color : "    ")}Create Project \u001b[0m");
-            Console.WriteLine($"{(option == 3 ? color : "    ")}Add time report on the project \u001b[0m");
-            Console.WriteLine($"{(option == 4 ? color : "    ")}Edit Person \u001b[0m");
-            Console.WriteLine($"{(option == 5 ? color : "    ")}Edit Project \u001b[0m");
-            Console.WriteLine($"{(option == 6 ? color : "    ")}Edit time report \u001b[0m");
-
-            // read which key has pressed by the user
-            key = Console.ReadKey(true);
+            // Get cursor position
+            (int left, int top) = Console.GetCursorPosition();
+            string color = "✅  \u001b[32m";
 
 
-            switch (key.Key)
+            Console.CursorVisible = false;
+
+
+            while (!isSelected)
             {
-                case ConsoleKey.DownArrow:
-                    option = (option==6 ? 1 : option + 1);
-                    break;
+                Console.SetCursorPosition(left, top);
 
-                case ConsoleKey.UpArrow:
-                    option = (option == 1 ? 6 : option - 1);
-                    break;
+                Console.WriteLine($"\n{(option == 1 ? color : "    ")}Create Person \u001b[0m");
+                Console.WriteLine($"{(option == 2 ? color : "    ")}Create Project \u001b[0m");
+                Console.WriteLine($"{(option == 3 ? color : "    ")}Add time report on the project \u001b[0m");
+                Console.WriteLine($"{(option == 4 ? color : "    ")}Edit Person \u001b[0m");
+                Console.WriteLine($"{(option == 5 ? color : "    ")}Edit Project \u001b[0m");
+                Console.WriteLine($"{(option == 6 ? color : "    ")}Edit time report \u001b[0m");
+                Console.WriteLine($"{(option == 7 ? color : "    ")}Terminate the program \u001b[0m");
 
-                case ConsoleKey.Enter:
-                    isSelected = true;
-                    break;
+                // read which key has pressed by the user
+                key = Console.ReadKey(true);
 
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.DownArrow:
+                        option = (option == 7 ? 1 : option + 1);
+                        break;
+
+                    case ConsoleKey.UpArrow:
+                        option = (option == 1 ? 7 : option - 1);
+                        break;
+
+                    case ConsoleKey.Enter:
+                        isSelected = true;
+                        break;
+
+                }
             }
-        }
 
-        Console.WriteLine($"\n{color}You selected the option {option}\n");
+            Console.WriteLine($"\n{color}You selected the option {option}\n");
 
-        switch (option)
-        {
-            case 1:
-                // Call CreatePerson method from service class to create person in tz_person table 
-                Service.CreatePerson();
-                break;
+            switch (option)
+            {
+                case 1:
+                    // Call CreatePerson method from service class to create person in tz_person table 
+                    Service.CreatePerson();
+                    break;
 
-            case 2:
-                // Create project in tz_project table 
-                Service.CreateProject();
-                break;
+                case 2:
+                    // Create project in tz_project table 
+                    Service.CreateProject();
+                    break;
 
-            case 3:
-                // Create time report  in tz_project_person table 
-                Service.TimeReport();
-                break;
+                case 3:
+                    // Create time report  in tz_project_person table 
+                    Service.TimeReport();
+                    break;
 
-            case 4:
-                // Call editPerson method from service class to update person in tz_person table 
-                Service.editPerson();
-                break;
+                case 4:
+                    // Call editPerson method from service class to update person in tz_person table 
+                    Service.editPerson();
+                    break;
 
-            case 5:
-                // update project in tz_project table 
-                Service.editProject();
-                break;
+                case 5:
+                    // update project in tz_project table 
+                    Service.editProject();
+                    break;
 
-            case 6:
-                // update time report  in tz_project_person table 
-                Service.editTimeReport();
-                break;
+                case 6:
+                    // update time report  in tz_project_person table 
+                    Service.editTimeReport();
+                    break;
+                case 7:
+                    isRunning = false;
+                    break;
 
-            default:
-                Console.WriteLine("Choose a valid option");
-                break;
+                default:
+                    Console.WriteLine("Choose a valid option");
+                    break;
+            }
+
+            
+            if (isRunning == true)
+            {
+                Console.WriteLine("Please press enter to continue");
+                Console.ReadKey();
+                // clear the console for a new retry
+                Console.Clear();
+            }
+
         }
 
     }
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # MiniProject
-Mini project  with postgressql
+## Create a application where a user can add time report of their project work and store data to postgressql database
+
+The goals of the project is to create a simple time reporting application where a consultant add their work hours on different projects
+they work on. Those reports can be also updated afterwards. No user login functionality required to minimise the scope. Learning how to work
+with multiple database tables and their relationship is the primary goal of the assignment.
+
+
+
+### Functionalities
+
+* Added a functionalities where user can select the menu using keyboard up and down arrow.
+* Create a person to  table
+* Create a project to table
+* Create time report to table
+* Update person record
+* Update project record
+* Update time report record
+ 
+### Technology stack
+
+* C#
+* PostgressSQL database
+* Dapper
+* NpgSQL
+
+
+### Challanges
+Not taking id (primary key of the table) as user input was one of the requirement. If any user enter a project name with a lot characters, it would be
+hard to type it when they want to update the record again.
+
+To update record from tz_project_person table, I allowed to user to input project_id and person_id to target a unique record of the table. 
+
+
+### To be improved
+
+* Application should allow to add multiple entry of time report for same project.
